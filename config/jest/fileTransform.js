0ba7684bd14e90f8dd40bedee9390b97d19269ef
sv.volkoff@gmail.com <sv.volkoff@gmail.com>
2019-01-09 13:48:32 +0400

    Add test infrastructure

---
+++ config/jest/fileTransform.js
@@ -0,0 +1,11 @@
+'use strict';
+
+const path = require('path');
+
+module.exports = {
+  process(src, filename) {
+    const assetFilename = JSON.stringify(path.basename(filename));
+
+    return `module.exports = ${assetFilename};`;
+  },
+};
