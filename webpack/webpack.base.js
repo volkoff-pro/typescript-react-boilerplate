2699140e4afc45a95606e050a039a3c6d55bb8c4
sv.volkoff@gmail.com <sv.volkoff@gmail.com>
2019-01-09 16:30:55 +0400

    CSS Modules

--- webpack/webpack.base.js
+++ webpack/webpack.base.js
@@ -1,7 +1,48 @@
 /* tslint:disable */
 const webpack = require('webpack');
 const paths = require('./paths');
+const autoprefixer = require('autoprefixer');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+const CSSModuleLoader = {
+  loader: 'typings-for-css-modules-loader',
+  options: {
+    modules: true,
+    importLoaders: 2,
+    sourceMap: true,
+    localIdentName: '[path][name]__[local]__[hash:base64:5]',
+    minimize: true,
+    namedExport: true,
+    camelCase: true,
+    sass: true
+  }
+}
+
+const CSSLoader = {
+  loader: 'css-loader',
+  options: {
+    modules: false,
+    importLoaders: 2,
+    sourceMap: true,
+    minimize: true
+  }
+}
+
+const PostCSSLoader = {
+  loader: 'postcss-loader',
+  options: {
+    ident: 'postcss',
+    sourceMap: true,
+    plugins: () => [
+      require('postcss-flexbugs-fixes'),
+      autoprefixer({
+        browsers: ['>1%', 'last 4 versions', 'Firefox ESR', 'not ie < 9'],
+        flexbox: 'no-2009'
+      })
+    ]
+  }
+}
 
 module.exports = {
   entry: [paths.appIndexTs],
@@ -26,9 +67,48 @@ module.exports = {
         ]
       },
       {
-        exclude: /node_modules/,
+        test: /\.(ts|tsx)$/,
         loader: require.resolve('ts-loader'),
-        test: /\.(ts|tsx)$/
+        exclude: /node_modules/
+      },
+      {
+        test: /\.s?css$/,
+        exclude: /\.module\.s?css$/,
+        use: [
+          {
+            loader: MiniCssExtractPlugin.loader,
+            options: {
+              sourceMap: true
+            }
+          },
+          CSSLoader,
+          PostCSSLoader,
+          {
+            loader: 'sass-loader',
+            options: {
+              sourceMap: true
+            }
+          }
+        ]
+      },
+      {
+        test: /\.module\.s?css$/,
+        use: [
+          {
+            loader: MiniCssExtractPlugin.loader,
+            options: {
+              sourceMap: true
+            }
+          },
+          CSSModuleLoader,
+          PostCSSLoader,
+          {
+            loader: 'sass-loader',
+            options: {
+              sourceMap: true
+            }
+          }
+        ]
       }
     ]
   },
@@ -39,9 +119,13 @@ module.exports = {
     }),
     new webpack.HashedModuleIdsPlugin(),
     new webpack.NamedModulesPlugin(),
-    new webpack.ProgressPlugin()
+    new webpack.ProgressPlugin(),
+    new webpack.WatchIgnorePlugin([
+      /css\.d\.tx$/,
+      /scss\.d\.tx$/,
+    ])
   ],
   resolve: {
-    extensions: ['.ts', '.tsx', '.js', '.jsx']
+    extensions: ['.ts', '.tsx', '.js', '.jsx', '.css', '.scss']
   }
 };
