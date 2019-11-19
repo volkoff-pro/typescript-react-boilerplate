2024ca008b84e2d06f55db912ca6e2656e37b620
sv.volkoff@gmail.com <sv.volkoff@gmail.com>
2019-11-19 14:54:05 +0400

    Add sample test

---
+++ src/components/App.test.tsx
@@ -0,0 +1,10 @@
+describe('Sample Test for CI pipeline', () => {
+  it('should return correct value', () => {
+    const a = 1;
+    const b = 2;
+
+    const result = a + b;
+
+    expect(result).toBe(3);
+  });
+});
